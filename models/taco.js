882ee1f92f1f2aed1f893bed0821d050dbff72a2
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-11-09 10:26:01 -0500

    Create model

---
+++ models/taco.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const tacoSchema = new Schema ({
+  title: String,
+  tasty: Boolean
+})
+
+const Taco = mongoose.model('Taco', tacoSchema)
+
+export {
+  Taco
+}
